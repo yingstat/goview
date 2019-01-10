3179224151120fd5b1b84455a87a51c9885b7acb
ying ying <yingyingcao1128@gmail.com>
2019-01-10 14:14:37 +0100

    inital commit

---
+++ R/goview.R
@@ -0,0 +1,21 @@
+#' goview Function
+#'
+#' This function allows you to show the results of GO enrichment analysis.
+#' @param goresults the go results from gProfileR
+#' @keywords GO
+#' @author yingying
+#' @export
+#' @examples
+#' goview()
+
+goview <- function(goresults){
+  library(ggplot2)
+  ggplot(data=goresults, aes(x=reorder(term.name, -p.value), y=overlap.size,fill=p.value)) +
+    geom_bar(stat="identity")+
+    scale_fill_gradient2(mid='red', high='blue', space='Lab')+
+    labs(title="GO enrichment",
+         x ="GO terms", y = "number of genes", fill = "P value")+
+    theme(plot.title = element_text(hjust = 0.5, size = 12))+
+    coord_flip()
+}
+
